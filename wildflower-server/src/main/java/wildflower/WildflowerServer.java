8c7175686a5cf896490dfa481117580143821ca7
Izaak Baker <ibaker@palantir.com>
2016-10-08 15:30:32 -0700

    added endpoints very alpha

--- wildflower-server/src/main/java/wildflower/WildflowerServer.java
+++ wildflower-server/src/main/java/wildflower/WildflowerServer.java
@@ -1,12 +1,17 @@
 package wildflower;
 
+import wildflower.creature.CreatureState;
+import wildflower.creature.CreatureObservation;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.UUID;
+import java.util.Set;
 
 import org.joml.Vector2f;
 
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import static spark.Spark.*;
 
@@ -26,14 +31,40 @@ public class WildflowerServer {
         staticFiles.location("/public");
         webSocket("/wildflower", WildflowerWebSocket.class);
 
-        post("/creature", (request, response) -> {
+        post("/api/v0/creature", (request, response) -> {
             JSONObject payload = new JSONObject(request.body());
             JSONObject location = payload.getJSONObject("location");
             Vector2f locationVector = new Vector2f((float)location.getDouble("x"), (float)location.getDouble("y"));
             return world.addCreature(locationVector);
         });
 
-        post("/creature/:id/move", (request, response) -> {
+        get("/api/v0/creature/:id", (request, response) -> {
+            UUID id = UUID.fromString(request.params("id"));
+            CreatureState creatureState = world.getCreatureState(id);
+            JSONObject creatureLocation = new JSONObject();
+            creatureLocation.put("x", creatureState.getLocation().x);
+            creatureLocation.put("y", creatureState.getLocation().y);
+            JSONObject creatureData = new JSONObject();
+            creatureData.put("location", creatureLocation);
+            return creatureData.toString();
+        });
+
+        get("/api/v0/creature/:id/observation", (request, response) -> {
+            UUID id = UUID.fromString(request.params("id"));
+            Set<CreatureObservation> creatureObservations = world.getSurroundingCreatures(id);
+            JSONArray creatures = new JSONArray();
+            creatureObservations.forEach(creatureObservation -> {
+                JSONObject location = new JSONObject();
+                location.put("x", creatureObservation.getLocation().x);
+                location.put("y", creatureObservation.getLocation().y);
+                JSONObject observationData = new JSONObject();
+                observationData.put("location", location);
+                creatures.put(observationData);
+            });
+            return creatures.toString();
+        });
+
+        post("/api/v0/creature/:id/move", (request, response) -> {
             UUID id = UUID.fromString(request.params("id"));
             JSONObject payload = new JSONObject(request.body());
             JSONObject force = payload.getJSONObject("force");
