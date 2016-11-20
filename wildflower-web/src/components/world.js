2f5c6d4b3737fb009adc05d3359eb07980a2f711
jason-h-hu <jason_hu@brown.edu>
2016-11-20 10:38:36 -0800

    save old wf-web

--- wildflower-web/src/components/world.js
+++ wildflower-web/src/components/world.js
@@ -2,15 +2,32 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import * as _ from 'lodash';
 import * as THREE from 'three';
+import { Renderer, Scene, Mesh } from 'react-three';
 
 export class World extends React.Component {
-    render() {
-      return <p>{JSON.stringify(this.props.terrain)}</p>
-    }
+  constructor() {
+    super();
+    this.scene = new THREE.Scene();
+    var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+    var VIEW_ANGLE = 45;
+    var ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
+    var NEAR = 0.1;
+    var FAR = 20000;
+    this.camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+    this.scene.add(this.camera);
+    this.camera.position.set(0,150,400);
+    this.camera.lookAt(this.scene.position);
+
+    this.renderer = new THREE.WebGLRenderer({ antialias:true });
+    this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  }
+
+  render() {
+    return <div />;
+  }
 }
 
 function mapStateToProps(state) {
-  console.log(state)
   return _.pick(state, ['terrain']);
 }
 
